a38391a7df7b6f80fa4853d9d4dc9a560c074109
Delia Alvarez <deliamalvarez@endava.com>
2020-07-24 12:37:52 -0300

    items copied

--- exercises/delia_solution/integer_list/integers.go
+++ exercises/delia_solution/integer_list/integers.go
@@ -19,11 +19,29 @@ func (l *IntegerList) removeFirstItem() {
 	}
 }
 
+func (l *IntegerList) checkItemExists(i int) bool {
+	for _, item := range l.list {
+		if item == i {
+			return true
+		}
+	}
+	return false
+}
+
+func (l *IntegerList) copyList() []int {
+	var cp = make([]int, len(l.list))
+	copy(cp, l.list)
+	return cp
+}
+
 func main() {
 	var b IntegerList
 	b.list = []int {4, 6, 8, 10}
 	b.insertItemAtFirst(2)
-	fmt.Println(b.list)
+	fmt.Println("After insert item at first: ", b.list)
 	b.removeFirstItem()
-	fmt.Println(b.list)
+	fmt.Println("After removing first item", b.list)
+	fmt.Println("Check if item 4 exists: ", b.checkItemExists(4))
+	fmt.Println("Check if item 20 exists: ", b.checkItemExists(20))
+	fmt.Println("Items copied", b.copyList())
 }
