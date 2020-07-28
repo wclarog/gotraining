b4b4750d86f52a30cc1d64f6eb43e96398d502a6
Roberto Assandri <roberto.assandri@gmail.com>
2020-07-28 16:02:03 -0300

    Deleted exercise description in teenager

--- exercises/RobertoAssandri/TheTeenager/TheTeenagerProgram.go
+++ exercises/RobertoAssandri/TheTeenager/TheTeenagerProgram.go
@@ -83,18 +83,3 @@ func main() {
 	fmt.Println("Answer should be: 'Whatever.'")
 	fmt.Println("Result is " + GetBobResponse(AnalyzePhrase("Hello")))
 }
-
-
-//The teenager
-//
-//Bob is a lackadaisical teenager. In conversation, his responses are very limited.
-//
-//Bob answers 'Sure.' if you ask him a question, such as "How are you?".
-//
-//He answers 'Whoa, chill out!' if you YELL AT HIM (in all capitals).
-//
-//He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
-//
-//He says 'Fine. Be that way!' if you address him without actually saying anything.
-//
-//He answers 'Whatever.' to anything else.
