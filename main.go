56277257194a90930d42e702d85c0ffe57a13112
wclaro <william.claro@endava.com>
2020-09-07 14:22:54 -0300

    error handling

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	practice22 "github.com/wclarog/gotraining/exercises/camila/practice2"
-)
-
 func main() {
-	fmt.Println("Leap years")
-	practice22.PrintLeapYears(1984, 2000)
-	fmt.Println("The teenager")
-	practice22.TestAnwers()
-	fmt.Println("Armstrong number")
-	practice22.TestArmstrongNumber()
+
 }
